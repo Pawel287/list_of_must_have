46d58a88b407f764a7802437f83af6a24ba7913c
Pawel287 <64600664+Pawel287@users.noreply.github.com>
2021-09-16 04:54:18 +0200

    add notes list screen (#3)
    * add notes list screen
    
    * various experiments

---
+++ lib/model/note.dart
@@ -0,0 +1,29 @@
+/*
+import 'dart:html';
+
+import 'package:list_of_must_have/model/note.dart';
+
+class Note {
+  int _id;
+  String _title;
+  String _content;
+  int _creationDate;
+
+
+  Note(this._id, this._title, this._content, this._creationDate);
+
+  int get id => _id;
+
+  String get title => _title;
+
+  String get content => _content;
+
+  int get creationDate => _creationDate;
+
+  String get date {
+    final date = DateTime.fromMillisecondsSinceEpoch(creationDate);
+    return DateFormat('EEE h:mm a, dd/MM/yyyy').format(date);
+  }
+}
+*/
+
