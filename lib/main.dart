b56081c578e5c438fc6fc90aa8d470ab4b3ac91c
Pawel287 <pawel1998wp.pl@wp.pl>
2021-09-16 04:37:23 +0200

    various experiments

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:list_of_must_have/screens/new_note/new_note_screen.dart';
 import 'package:list_of_must_have/screens/notes_list/notes_list_screen.dart';
 import 'package:list_of_must_have/model/note.dart';
 
@@ -9,16 +10,26 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    const title = 'Wszystkie notatki';
+    return ChangeNotifierProvider<NotesOperation>(
+        create: (context) => NotesOperation(),
+        child: MaterialApp(
+          home: NotesListScreen(),
+        )
+    );
+  }
+}
 
-    return MaterialApp(
+ /*   return MaterialApp(
       title: title,
+
       home: Scaffold(
         appBar: AppBar(
           title: const Text(title),
+          centerTitle: true,
         ),
         body: NotesListScreen(),
       ),
     );
   }
 }
+*/
